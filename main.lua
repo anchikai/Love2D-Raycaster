4fb7e4d3ee1322229c6f15517e77bb25c32400ea
anchikai <hentaisuperstar86@gmail.com>
2022-08-14 23:40:21 -0400

    Added strafing left and right and camera movement with mouse

--- main.lua
+++ main.lua
@@ -9,24 +9,28 @@ function love.load()
     -- Window Shit
     love.graphics.setBackgroundColor(0.3, 0.3, 0.3)
     love.window.setMode(1024, 512)
+    CenterX = love.graphics.getWidth()/2
+    CenterY = love.graphics.getHeight()/2
+    love.mouse.setVisible(false)
 
     -- Player
     Player = {}
     Player.Angle = 0
-    Player.X, Player.Y = 512, 256
+    Player.X, Player.Y = 96, CenterY
     Player.DeltaX, Player.DeltaY = math.cos(Player.Angle)*5, math.sin(Player.Angle)*5
+    Player.DeltaX2, Player.DeltaY2 = math.cos(Player.Angle-(math.pi/2))*5, math.sin(Player.Angle-(math.pi/2))*5
 
     -- Map/World
     Map = {}
     Map.X, Map.Y, Map.Size = 16, 8, 64
     Map.Grid = {
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-        1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1,
-        1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1,
-        1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
-        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1,
+        1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1,
+        1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
     }
 
@@ -37,24 +41,47 @@ end
 local rayCooldown, resetCooldown = 0, 0
 function love.update(dt)
     -- Player Movement
-    if love.keyboard.isDown("a") then
-        Player.Angle = Player.Angle - 0.1
+    if love.mouse.getX() < CenterX then
+        Player.Angle = Player.Angle - 0.066
         if Player.Angle < 0 then
             Player.Angle = Player.Angle + 2*math.pi
         end
         Player.DeltaX = math.cos(Player.Angle)*5
         Player.DeltaY = math.sin(Player.Angle)*5
+        Player.DeltaX2 = math.cos(Player.Angle-(math.pi/2))*5
+        Player.DeltaY2 = math.sin(Player.Angle-(math.pi/2))*5
     end
-    if love.keyboard.isDown("d") then
-        Player.Angle = Player.Angle + 0.1
+    if love.mouse.getX() > CenterX then
+        Player.Angle = Player.Angle + 0.066
         if Player.Angle > 2*math.pi then
             Player.Angle = Player.Angle - 2*math.pi
         end
         Player.DeltaX = math.cos(Player.Angle)*5
         Player.DeltaY = math.sin(Player.Angle)*5
+        Player.DeltaX2 = math.cos(Player.Angle-(math.pi/2))*5
+        Player.DeltaY2 = math.sin(Player.Angle-(math.pi/2))*5
     end
-    if love.keyboard.isDown("w") then Player.X = Player.X + Player.DeltaX/2 Player.Y = Player.Y + Player.DeltaY/2 end
-    if love.keyboard.isDown("s") then Player.X = Player.X - Player.DeltaX/2 Player.Y = Player.Y - Player.DeltaY/2 end
+
+    -- Forward/Backward
+    if love.keyboard.isDown("w") then
+        Player.X = Player.X + Player.DeltaX/2
+        Player.Y = Player.Y + Player.DeltaY/2
+    end
+    if love.keyboard.isDown("s") then
+        Player.X = Player.X - Player.DeltaX/2
+        Player.Y = Player.Y - Player.DeltaY/2
+    end
+
+    -- Left/Right
+    if love.keyboard.isDown("a") then
+        Player.X = Player.X + Player.DeltaX2/2
+        Player.Y = Player.Y + Player.DeltaY2/2
+    end
+    if love.keyboard.isDown("d") then
+        Player.X = Player.X - Player.DeltaX2/2
+        Player.Y = Player.Y - Player.DeltaY2/2
+    end
+    print(Player.X, Player.Y)
 
     -- Switch Render Mode
     if love.keyboard.isDown("2") then RenderType = 2 end
@@ -78,6 +105,9 @@ function love.update(dt)
         resetCooldown = 1
     else
         resetCooldown = 0
+    end
+    if love.window.hasFocus() then
+        love.mouse.setPosition(CenterX, CenterY)
     end
 end
 
--- reset.lua
+++ reset.lua
@@ -1,5 +1,6 @@
 function reset()
     Player.Angle = 0
-    Player.X, Player.Y = 512, 256
+    Player.X, Player.Y = 96, CenterY
     Player.DeltaX, Player.DeltaY = math.cos(Player.Angle)*5, math.sin(Player.Angle)*5
+    Player.DeltaX2, Player.DeltaY2 = math.cos(Player.Angle-(math.pi/2))*5, math.sin(Player.Angle-(math.pi/2))*5
 end
